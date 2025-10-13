afe05dc7ee4fd87ada66ae947770033b88ee1cb0
Bjorn Beishline <75190918+BjornTheProgrammer@users.noreply.github.com>
2025-10-13 13:47:05 -0700

    Make build work

--- packages/cli/src/cli/build.ts
+++ packages/cli/src/cli/build.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import { createJiti } from "jiti";
 import type { Bf6Config } from "../resources/prepare/types/config.ts";
 import { rolldown } from "rolldown";
+import fs from 'fs';
 
 declare global {
 	var defineBf6Config: ((config: Bf6Config) => Bf6Config) | undefined;
@@ -27,14 +28,41 @@ export async function build() {
 		? config.minify
 		: config.minify?.json ?? false;
 
+	const outDir = path.resolve(workingDir, config.outDir);
+	if (config.entrypoint) buildTs(path.resolve(workingDir, config.entrypoint), outDir, minifyJs);
+	if (config.strings) buildStrings(path.resolve(workingDir, config.strings), outDir, minifyJson);
+	if (config.config) buildConfig(path.resolve(workingDir, config.config), outDir, minifyJson);
+	if (config.tscn) buildTscn(path.resolve(workingDir, config.tscn), outDir);
+}
+
+async function buildTs(entrypoint: string, outDir: string, minify: boolean) {
 	const bundle = await rolldown({
-		input: config.entrypoint,
+		input: entrypoint,
 	});
 
 	await bundle.write({
 		format: 'esm',
-		file: `${config.outDir}/index.ts`,
+		file: `${outDir}/index.ts`,
 		inlineDynamicImports: true,
-		minify: minifyJs
+		minify
 	});
+}
+
+async function buildStrings(entrypoint: string, outDir: string, minify: boolean) {
+	if (!fs.existsSync(entrypoint)) throw new Error('Cannot find strings file');
+	let output = await fs.promises.readFile(entrypoint, { encoding: 'utf8' });
+	if (minify) output = JSON.stringify(JSON.parse(output));
+	await fs.promises.writeFile(path.resolve(outDir, 'strings.json'), output);
+}
+
+async function buildConfig(entrypoint: string, outDir: string, minify: boolean) {
+	if (!fs.existsSync(entrypoint)) throw new Error('Cannot find config file');
+	let output = await fs.promises.readFile(entrypoint, { encoding: 'utf8' });
+	if (minify) output = JSON.stringify(JSON.parse(output));
+	await fs.promises.writeFile(path.resolve(outDir, 'config.json'), output);
+}
+
+async function buildTscn(entrypoint: string, outDir: string) {
+	if (!fs.existsSync(entrypoint)) throw new Error('Cannot find tscn file');
+	await fs.promises.copyFile(entrypoint, path.resolve(outDir, 'levels.tscn'));
 }
