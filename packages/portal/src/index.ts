be008aab87a598ebd9bc8f64c2de2506a69d2ac7
Bjorn Beishline <75190918+BjornTheProgrammer@users.noreply.github.com>
2025-10-25 02:28:14 -0700

    Get clients working for protobufs

--- packages/portal/src/index.ts
+++ packages/portal/src/index.ts
@@ -1,15 +1,38 @@
 import { createClient } from "@connectrpc/connect";
-import { WebPlayServiceV1Service } from "../gen/santiago/web/play/v1/web_play_service_pb";
-import { createConnectTransport } from "@connectrpc/connect-node";
+import { WebPlay } from "../gen/santiago/web/play/WebPlay_pb";
+import { WebAuthentication } from "../gen/santiago/web/authentication/WebAuthentication_pb";
+import { createGrpcWebTransport } from "@connectrpc/connect-web";
 
-const transport = createConnectTransport({
-    baseUrl: "https://santiago-prod-wgw-envoy.ops.dice.se/santiago.web.play.WebPlay",
-    httpVersion: "1.1"
+const transport = createGrpcWebTransport({
+  baseUrl: "https://santiago-prod-wgw-envoy.ops.dice.se",
+  useBinaryFormat: true,
+  interceptors: [
+    (next) => async (req) => {
+      req.header.set("x-dice-tenancy", "prod_default-prod_default-santiago-common");
+      req.header.set("x-gateway-session-id", "web-35b20027-6e20-4a84-b0e0-e63c2e3876eb");
+      req.header.set("x-grpc-web", "1");
+      return next(req);
+    }
+  ],
 });
 
-const webPlayClient = createClient(WebPlayServiceV1Service, transport);
+const webPlayClient = createClient(WebPlay, transport);
+const authenticationClient = createClient(WebAuthentication, transport);
 
-const res = await webPlayClient.say({
-  sentence: "I feel happy.",
+const auth = await authenticationClient.viaAuthCode({
+  authCode: "xxxxxxxxxxxxxxx",
+  platform: 1,
+  redirectUri: {
+    value: "https://portal.battlefield.com/bf6/experiences/"
+  }
+})
+
+console.log('auth:', auth);
+
+const res = await webPlayClient.getPlayElement({
+  id: "xxxxxxxxxxxxxxxxxxxxxxx",
+  includeDenied: true,
 });
-console.log(res.sentence);
+
+console.log(res);
+
