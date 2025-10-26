fcc521784d2c11e1e4f20ddf77681e3d73befe96
Bjorn Beishline <75190918+BjornTheProgrammer@users.noreply.github.com>
2025-10-26 13:29:21 -0700

    Fix protobufs and add exports

--- packages/portal/src/index.ts
+++ packages/portal/src/index.ts
@@ -3,13 +3,39 @@ import { WebPlay } from "../gen/santiago/web/play/WebPlay_pb";
 import { WebAuthentication } from "../gen/santiago/web/authentication/WebAuthentication_pb";
 import { createGrpcWebTransport } from "@connectrpc/connect-web";
 
-export function createClients(getAuthSession: () => string) {
+export * as WebAuthentication_pb from "../gen/santiago/web/authentication/WebAuthentication_pb";
+export * as WebPlay_pb from "../gen/santiago/web/play/WebPlay_pb";
+export * as Generated_pb from '../gen/santiago/common/generated_pb';
+
+/**
+ * Here is an example of using this to query for playelements and returning them
+ * ```ts
+ * // You can get the token by going to portal.battlefield.com and opening inspect element
+ * // and searching for any request like getPlayElement or getOwnedPlayElements
+ * // then copying it from the request header
+ * const clients = await createClients('web-XXXXXXXXXXXXXXXXXXXXXXXXX');
+ * const result = await clients.play.getOwnedPlayElements({
+ * 	publishStates: [1, 2, 4],
+ * 	includeDenied: true
+ * })
+
+ * const playElement = await clients.play.getPlayElement({
+ * 	id: result.playElements[0].id,
+ * 	includeDenied: true,
+ * })
+
+ * console.log('result:', result);
+ * console.log('playElement:', playElement);
+ * ```
+ */
+export async function createClients(getAuthSession: (() => Promise<string> | string) | string) {
+	const session = typeof getAuthSession === 'string' ? getAuthSession : await getAuthSession();
 	const transport = createGrpcWebTransport({
 		baseUrl: "https://santiago-prod-wgw-envoy.ops.dice.se",
 		interceptors: [
 			next => async req => {
 				req.header.set("x-dice-tenancy", "prod_default-prod_default-santiago-common");
-				req.header.set("x-gateway-session-id", getAuthSession());
+				req.header.set("x-gateway-session-id", session);
 				return next(req);
 			}
 		]
