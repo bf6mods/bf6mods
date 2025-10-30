010c53225e25573c61b4575e158528a596954900
Bjorn Beishline <75190918+BjornTheProgrammer@users.noreply.github.com>
2025-10-30 02:34:48 -0700

    Fix lints

--- packages/portal/src/index.ts
+++ packages/portal/src/index.ts
@@ -2,17 +2,15 @@ import { createClient } from "@connectrpc/connect";
 import { createGrpcWebTransport } from "@connectrpc/connect-web";
 import { WebAuthentication } from "../gen/santiago/web/authentication/WebAuthentication_pb";
 import { WebPlay } from "../gen/santiago/web/play/WebPlay_pb";
-import { GetBlueprintsByIdResponseSchema, GetScheduledBlueprintsResponseSchema, UpdatePlayElementRequestSchema } from "../gen/santiago/common/generated_pb";
-import * as protobufBuild from "@bufbuild/protobuf";
-import { BinaryReader, WireType } from "@bufbuild/protobuf/wire";
+
 export * as protobuf from "@bufbuild/protobuf";
 export * as Generated_pb from "../gen/santiago/common/generated_pb";
 export * as WebAuthentication_pb from "../gen/santiago/web/authentication/WebAuthentication_pb";
 export * as WebPlay_pb from "../gen/santiago/web/play/WebPlay_pb";
 
 export type AuthOptions =
-  | { authCode: string; sessionId?: string }
-  | { authCode?: string; sessionId: string };
+	| { authCode: string; sessionId?: string }
+	| { authCode?: string; sessionId: string };
 
 /**
  * Here is an example of using this to query for playelements and returning them
@@ -40,18 +38,15 @@ export class Clients {
 	transport = createGrpcWebTransport({
 		baseUrl: "https://santiago-prod-wgw-envoy.ops.dice.se",
 		interceptors: [
-			next => async req => {
-				req.header.set("x-dice-tenancy", "prod_default-prod_default-santiago-common");
+			(next) => async (req) => {
+				req.header.set(
+					"x-dice-tenancy",
+					"prod_default-prod_default-santiago-common",
+				);
 				if (this.session) req.header.set("x-gateway-session-id", this.session);
 				return next(req);
 			},
 		],
-		fetch: async (...args: Parameters<typeof globalThis.fetch>): ReturnType<typeof globalThis.fetch> => {
-			const response = await globalThis.fetch(...args);
-			const buf = new Uint8Array(await response.clone().arrayBuffer());
-			fs.writeFileSync('./bytes.txt', buf.slice(5, -1).slice(0, -19));
-			return response;
-		}
 	});
 
 	auth = createClient(WebAuthentication, this.transport);
@@ -63,8 +58,8 @@ export class Clients {
 			const res = await this.auth.viaAuthCode({
 				authCode: options.authCode,
 				platform: 1,
-				redirectUri: "https://portal.battlefield.com/bf6/experiences/"
-			})
+				redirectUri: "https://portal.battlefield.com/bf6/experiences/",
+			});
 
 			this.session = res.sessionId;
 		}
