586c5add1847ca662d55fd5b6e68d70c45adbd8c
tomaytotomato <971697+tomaytotomato@users.noreply.github.com>
2025-12-29 16:52:01 +0000

    FEAT - renaming template folder
    Adding option in CLI
    
    FEAT - Add comprehensive template

---
+++ packages/cli/src/resources/templates/Complete/bf6.config.ts
@@ -0,0 +1,8 @@
+export default defineBf6Config({
+    outDir: 'dist',
+    entrypoint: 'src/index.ts',
+    name: '{bf6ConfigName}',
+    description: 'A complete template with all in-game event handlers',
+    scenes: [MapId.EmpireState],
+    game: {},
+})
